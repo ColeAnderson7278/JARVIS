235efad4c1ba089c5ad646fccb87a7bbc6caf200
ColeAnderson7278 <canderson@basecampcodingacademy.org>
2018-11-12 13:08:19 -0600

    Showing Weather to User
    We are showing the user the weather, temperature, and the
    wind speed as of now. Our goal now is to make a better UI and
    to show more data.

--- shell.py
+++ shell.py
@@ -1,8 +1,19 @@
 import weather
+import json
+
+secrets = json.load(open("secrets.json"))
+
+
+def show_message():
+    print(f"Hello {secrets['UserInfo']['name']}\n")
+    print(
+        f"Weather:\n{weather.get_description()}\nTemp: {weather.get_temperature()}°F | Wind: {weather.get_wind_speed()} mph"
+    )
 
 
 def main():
-    print(weather.get_temperature())
+    show_message()
+    # print(weather.info())
 
 
 if __name__ == '__main__':
--- weather.py
+++ weather.py
@@ -1,12 +1,29 @@
 import requests
 import json
+import math
 
 secrets = json.load(open("secrets.json"))
 
 r = requests.get(
-    f"http://api.openweathermap.org/data/2.5/weather?zip={secrets['ZIP_Code']},us&APPID={secrets['WeatherAPI_Key']}"
+    f"http://api.openweathermap.org/data/2.5/weather?zip={secrets['WeatherInfo']['ZIP_Code']},us&APPID={secrets['WeatherInfo']['WeatherAPI_Key']}"
 )
 
 
 def get_temperature():
+    return math.floor((int(r.json()["main"]["temp"]) - 273.15) * (9 / 5) + 32)
+
+
+def get_description():
+    words = r.json()["weather"][0]["description"].split(" ")
+    new = ""
+    for word in words:
+        new += f"{word.capitalize()} "
+    return new
+
+
+def get_wind_speed():
+    return r.json()["wind"]["speed"]
+
+
+def info():
     return r.text
