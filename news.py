d867fa8fdf30a15bb6ee283120cc6bd4150d1bd6
ColeAnderson7278 <canderson@basecampcodingacademy.org>
2018-11-13 11:59:30 -0600

    Added News Section
    We found an API for news headlines that we then pulled the headline,
    image, and link for three different new stories. We've now displayed
    it to the user, but still have to format it better.

---
+++ news.py
@@ -0,0 +1,45 @@
+import requests
+import json
+
+secrets = json.load(open("secrets.json"))
+
+
+class NewsAPI:
+    DEFAULT_COUNTRY = secrets['NewsInfo']['country']
+    URL_FORMAT = "https://newsapi.org/v2/top-headlines?pageSize=3&country={country}&apiKey=" + secrets[
+        'NewsInfo']['NewsAPI_Key']
+
+    def __init__(self, country=None):
+        self.country = country or self.DEFAULT_COUNTRY
+
+    @property
+    def url(self):
+        return self.URL_FORMAT.format(country=self.country)
+
+    @property
+    def get(self):
+        response = requests.get(self.url)
+        data = response.json()
+        return News(
+            articleOne={
+                'headline': data['articles'][0]['title'],
+                'image_URL': data['articles'][0]['urlToImage'],
+                'link': data['articles'][0]['url']
+            },
+            articleTwo={
+                'headline': data['articles'][1]['title'],
+                'image_URL': data['articles'][1]['urlToImage'],
+                'link': data['articles'][1]['url']
+            },
+            articleThree={
+                'headline': data['articles'][2]['title'],
+                'image_URL': data['articles'][2]['urlToImage'],
+                'link': data['articles'][2]['url']
+            })
+
+
+class News:
+    def __init__(self, *, articleOne, articleTwo, articleThree):
+        self.articleOne = articleOne
+        self.articleTwo = articleTwo
+        self.articleThree = articleThree
--- shell.py
+++ shell.py
@@ -1,4 +1,5 @@
 import weather as w
+import news as n
 import json
 from flask import Flask, render_template
 
@@ -7,6 +8,7 @@ secrets = json.load(open("secrets.json"))
 app = Flask(__name__)
 
 weather = w.WeatherAPI().get
+news = n.NewsAPI().get
 
 
 def cli_main():
@@ -18,13 +20,17 @@ def web_main():
     return render_template(
         'root.html',
         context={
+            'userName': secrets['UserInfo']['name'],
             'description': weather.description,
             'temp': weather.temp_f,
             'wind': weather.wind,
             'humidity': weather.humidity,
             'temp_min': weather.temp_min,
             'temp_max': weather.temp_max,
-            'image_URL': weather.get_image_URL()
+            'image_URL': weather.get_image_URL(),
+            'articleOne': news.articleOne,
+            'articleTwo': news.articleTwo,
+            'articleThree': news.articleThree
         })
 
 
