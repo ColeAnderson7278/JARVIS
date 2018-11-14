ae6b635fdc9189e64838b347c24db5ffa1991db3
ColeAnderson7278 <canderson@basecampcodingacademy.org>
2018-11-14 09:24:44 -0600

    Added in City Finder
    Used another API to take in the given zipcode and use it to figure
    out what city the weather is being gathered from. Also added it
    to the HTML to display it with the weather info.

--- shell.py
+++ shell.py
@@ -1,6 +1,7 @@
 import weather as w
 import news as n
 import quote as q
+import zip_finder as z
 import json
 from flask import Flask, render_template
 
@@ -11,6 +12,7 @@ app = Flask(__name__)
 weather = w.WeatherAPI().get()
 news = n.NewsAPI().get()
 quote = q.QuoteAPI().get()
+zipcode = z.ZipFinderAPI().get()
 
 
 def cli_main():
@@ -30,6 +32,8 @@ def web_main():
             'weather_image_URL': weather.get_image_URL(),
             'articles': news.articles,
             'quote': quote,
+            'city': zipcode.city_info['city'],
+            'state': zipcode.city_info['state']
         })
 
 
